176fa766fd226af9ded76620e90b87e5aace4643
Vincent H Guyo <ghonestvincent@gmail.com>
2022-07-11 02:01:49 +0200

    Corrected the named query referenced table name.

--- electronic-payments-domain/src/main/java/com/econetwireless/epay/domain/SubscriberRequest.java
+++ electronic-payments-domain/src/main/java/com/econetwireless/epay/domain/SubscriberRequest.java
@@ -10,7 +10,10 @@ import java.util.Date;
  * Created by tnyamakura on 17/3/2017.
  */
 @Entity(name = "request")
-@NamedQueries({@NamedQuery(name = "SubscriberRequest.findByPartnerCode", query = "select r from Request r where r.partnerCode = :partnerCode order by r.dateCreated desc ")})
+/**
+ * 10. Changed the named query name from Request to request as specified on the entity annotation.
+ * */
+@NamedQueries({@NamedQuery(name = "SubscriberRequest.findByPartnerCode", query = "select r from request r where r.partnerCode = :partnerCode order by r.dateCreated desc ")})
 @Table(name = "e_request", indexes = {@Index(name = "req_msisdn_indx", columnList = "mobile_number")})
 public class SubscriberRequest {
     @Id
